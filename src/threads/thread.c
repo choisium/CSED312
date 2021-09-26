63483e7f4704f6f10a66022a3558377f3216dcb8
Gyumin Choi <kyuchoi95@postech.ac.kr>
2021-09-26 02:09:37 -0700

    Project1: fix thread_unblock(), thread_yield()

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -67,6 +67,9 @@ static void kernel_thread (thread_func *, void *aux);
 
 static bool comp_tick (const struct list_elem *, const struct list_elem *,
                         void *);
+static bool comp_priority (const struct list_elem *, const struct list_elem *,
+                        void *);
+
 static void idle (void *aux UNUSED);
 static struct thread *running_thread (void);
 static struct thread *next_thread_to_run (void);
@@ -246,7 +249,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, comp_priority, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -278,6 +281,17 @@ comp_tick (const struct list_elem *a_, const struct list_elem *b_,
   const struct thread *b = list_entry (b_, struct thread, elem);
 
   return a->wakeup_tick < b->wakeup_tick;
+};
+
+/* compare function for thread priority */
+static bool
+comp_priority (const struct list_elem *a_, const struct list_elem *b_,
+            void *aux UNUSED) 
+{
+  const struct thread *a = list_entry (a_, struct thread, elem);
+  const struct thread *b = list_entry (b_, struct thread, elem);
+
+  return a->priority > b->priority;
 };
 
 void 
@@ -360,7 +374,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, comp_priority, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
