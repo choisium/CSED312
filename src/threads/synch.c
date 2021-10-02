ed08753107e3ce4264425bc766a7046afa0e28f7
Gyumin Choi <kyuchoi95@postech.ac.kr>
2021-10-02 07:53:14 -0700

    Project1: Fix sema_up to sort sema->waiters

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -122,8 +122,11 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+    {
+      list_sort (&sema->waiters, comp_priority_greater, NULL);
+      thread_unblock (list_entry (list_pop_front (&sema->waiters),
+                                  struct thread, elem));
+    }
   sema->value++;
   intr_set_level (old_level);
 
