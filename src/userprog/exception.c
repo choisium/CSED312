c969b9de481e6f17384453ccd7c7b7eb5519fc7b
Soomin Choi <choisium@gmail.com>
2021-11-30 19:10:32 +0900

    lab3: remove printf used to debug

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -157,11 +157,13 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+/*
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
+*/
 
 #ifdef VM
   if (not_present)
@@ -184,16 +186,6 @@ page_fault (struct intr_frame *f)
 
 #else
   exit(-1);
-
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
-  kill (f);
 #endif
 }
 
